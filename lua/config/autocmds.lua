e7f8324212343f9800e2ffc32ef68d3d85a9800c
sabarish-vm <sabarish.science@gmail.com>
2024-03-03 23:30:26 +0100

    LazyVim first commit

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,9 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+-- Recognize a new filetype for OpenGadget3 parameter file
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*.g3par",
+  command = "setlocal filetype=g3par",
+})
